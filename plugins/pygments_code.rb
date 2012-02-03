e579b9ce94d539a280b5ad39d969e5539fd233a6
Wlodek Bzyl <matwb@ug.edu.pl>
2012-02-03 01:55:22 +0100

    Zmieniłem tło w code snippets na jasne.

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -30,12 +30,20 @@ module HighlightCode
     highlighted_code
   end
   def tableize_code (str, lang = '')
-    table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
+    table = '<div class="highlight">'
     code = ''
-    str.lines.each_with_index do |line,index|
-      table += "<span class='line-number'>#{index+1}</span>\n"
-      code  += "<span class='line'>#{line}</span>"
-    end
-    table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+    # str.lines.each_with_index do |line,index|
+    #   code  += "#{line}"
+    # end
+    table += "<pre><code class='#{lang}'>#{str}</code></pre></div>"
   end
+  # def tableize_code (str, lang = '')
+  #   table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
+  #   code = ''
+  #   str.lines.each_with_index do |line,index|
+  #     table += "<span class='line-number'>#{index+1}</span>\n"
+  #     code  += "<span class='line'>#{line}</span>"
+  #   end
+  #   table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+  # end
 end
